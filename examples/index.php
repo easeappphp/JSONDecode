dab94b2343b487631fc9f42ffab0adab33a40e84
raghuveer <raghuveer.dendukuri@gmail.com>
2021-03-07 16:47:44 +0530

    code simplification

--- examples/index.php
+++ examples/index.php
@@ -1,126 +1,91 @@
 <?php
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use \EaseAppPHP\JSONDecode\JSONDecode;
 
 $jsonDecode = new JSONDecode();
 
-var_dump($jsonDecode->isJson("{ 'bar': 'baz' }")); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('{ bar: "baz" }')); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('{ bar: "baz", }')); // bool(false) - CORRECT
+//$isJsonCheckInput = "{ 'bar': 'baz' }"; // bool(false)
+
+//$isJsonCheckInput = '{ bar: "baz" }'; // bool(false)
+ 
+//$isJsonCheckInput = '{ bar: "baz", }'; // bool(false)
+
+$isJsonCheckInput = '{"a":5}'; // bool(true)
+
+//$isJsonCheckInput = '[1,2,3]'; // bool(true)
+
+//$isJsonCheckInput = '11111111111'; // bool(false)
+
+//$isJsonCheckInput = '1.5'; // bool(false)
+
+//$isJsonCheckInput = 'true'; // bool(false)
+
+//$isJsonCheckInput = 'false'; // bool(false)
+
+//$isJsonCheckInput = 'null'; // bool(false)
+
+//$isJsonCheckInput = 'hello'; // bool(false)
+
+//$isJsonCheckInput = ''; // bool(false)
+
+//$isJsonCheckInput = '"hello"'; // bool(true)
+
+//$isJsonCheckInput = 11111111111; // bool(false)
+
+//$isJsonCheckInput = array(); // bool(false)
+
+//$isJsonCheckInput = new stdClass; // bool(false)
+
+//$isJsonCheckInput = '"42"'; // bool(true)
+
+//$isJsonCheckInput = '42'; // bool(false)
+
+echo "isJsonCheckInput: <br>";
+var_dump($isJsonCheckInput);
+
+echo "<br>=========================================================================<br>";
+
+$isJsonCheckResult = $jsonDecode->isJson($isJsonCheckInput);
+
+echo "<br>isJsonCheckResult: <br>";
+var_dump($isJsonCheckResult);
+
+echo "<br>=========================================================================<br>";
+echo "Output is given as Array<br>";
+
+echo "getJsonDecodedDataCheckResult: <br>";
+$receivedData = $jsonDecode->getJsonDecodedData($isJsonCheckInput, true);
+	
+if (($receivedData["is_json"] === true) && ($receivedData["content"] != "")) {
+	
+	$data = $receivedData["content"];
+	var_dump($data);
+	
+} else {
+	
+	var_dump($receivedData);
+	
+}
 echo "<br>";
 
-var_dump($jsonDecode->isJson('{"a":5}')); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('[1,2,3]')); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('11111111111')); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->isJson('1.5')); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->isJson('true')); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->isJson('false')); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('null')); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->isJson('hello')); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('')); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('"hello"')); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson(11111111111)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson(array())); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson(new stdClass)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('"42"')); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->isJson('42')); // bool(false) - CORRECT
+echo "<br>=========================================================================<br>";
+echo "Output is given as Object<br>";
+
+echo "getJsonDecodedDataCheckResult: <br>";
+$receivedData = $jsonDecode->getJsonDecodedData($isJsonCheckInput, false);
+	
+if (($receivedData["is_json"] === true) && ($receivedData["content"] != "")) {
+	
+	$data = $receivedData["content"];
+	var_dump($data);
+	
+} else {
+	
+	var_dump($receivedData);
+	
+}
 echo "<br>";
 
-echo "=========================================================================<br>";
-
-var_dump($jsonDecode->getJsonDecodedData("{ 'bar': 'baz' }", true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('{ bar: "baz" }', true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('{ bar: "baz", }', true)); // bool(false) - CORRECT
-echo "<br>";
-
-var_dump($jsonDecode->getJsonDecodedData('{"a":5}', true)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('[1,2,3]', true)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('11111111111', true)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('1.5', true)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('true', true)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('false', true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('null', true)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('hello', true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('', true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('"hello"', true)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(11111111111, true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(array(), true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(new stdClass, true)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('"42"', true)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('42', true)); // bool(false) - CORRECT
-echo "<br>";
-
-echo "=========================================================================<br>";
-
-var_dump($jsonDecode->getJsonDecodedData("{ 'bar': 'baz' }", false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('{ bar: "baz" }', false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('{ bar: "baz", }', false)); // bool(false) - CORRECT
-echo "<br>";
-
-var_dump($jsonDecode->getJsonDecodedData('{"a":5}', false)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('[1,2,3]', false)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('11111111111', false)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('1.5', false)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('true', false)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('false', false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('null', false)); // bool(false) - CORRECTED
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('hello', false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('', false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('"hello"', false)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(11111111111, false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(array(), false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData(new stdClass, false)); // bool(false) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('"42"', false)); // bool(true) - CORRECT
-echo "<br>";
-var_dump($jsonDecode->getJsonDecodedData('42', false)); // bool(false) - CORRECT
-echo "<br>";
 
 ?>
--- src/JSONDecode.php
+++ src/JSONDecode.php
@@ -11,7 +11,7 @@ use \EaseAppPHP\JSONDecode\Exceptions\JSONDecodeException;
 *
 * Author: Raghuveer Dendukuri
 *
-* Version: 1.0.2
+* Version: 1.0.3
 *
 * Description: This is to do json_decode operation only on valid json string and in a highly performing way.
 *
@@ -28,141 +28,96 @@ class JSONDecode {
 	public function isJson($value) {
 		try {
 			
-			if (!is_string($value)) {
-				// A non-string value is never a Valid JSON string.
+			/**
+			 * Considerations
+			 * A non-string value is never a Valid JSON string.
+			 * A numeric string in double quotes that is encapsulated in single quotes is a valid JSON string. Anything else is never a Valid JSON string.
+			 * Any non-numeric JSON string must be longer than 2 characters, or else, it cannot be considered for next level of JSON string validation checks.
+			 * "null" is never a valid JSON string.
+			 * "true" is never a valid JSON string.
+			 * "false" is never a valid JSON string.
+			 * Every JSON string has to be wrapped in {}, [] or "", if it is not, then that is never a Valid JSON string.
+			 */
+			if ((!is_string($value)) || (is_numeric($value)) || (mb_strlen($value) < 2) || ('null' === $value) || ('true' === $value) || ('false' === $value) || ('{' != $value[0] && '[' != $value[0] && '"' != $value[0])) {
+				
 				return false;
-				
-			} else if (is_numeric($value)) { 
-				// A numeric string in double quotes that is encapsulated in single quotes is a valid JSON string. Anything else is never a Valid JSON string.
-				return false; 
-				
-			} else if (mb_strlen($value) < 2) { 
-				// Any non-numeric JSON string must be longer than 2 characters, or else, it cannot be considered for next level of JSON string validation checks.
-				return false;
-				
-			} else if ('null' === $value) { 
-				// "null" is never a valid JSON string.
-				return false;
-				
-			} else if ('true' === $value) { 
-				// "true" is never a valid JSON string.
-				return false;
-				
-			} else if ('false' === $value) { 
-				// "false" is never a valid JSON string.
-				return false;
-				
-			} else if ( '{' != $value[0] && '[' != $value[0] && '"' != $value[0] ) { 
-				// Every JSON string has to be wrapped in {}, [] or "", if it is not, then that is never a Valid JSON string.
-				return false;
-				
-			} else {
-				//Actual JSON Decode operation using php's inbuilt json_decode function.
-				$json = json_decode($value);
-				return $json !== false && !is_null($json) && $value != $json;
 				
 			}
 			
+			//Actual JSON Decode operation using php's inbuilt json_decode function.
+			$json = json_decode($value);
+			
+			if (($json !== false) && ($json !== null) && ($value != $json)) {
+				
+				return true;
+				
+			}
+			
+			return false;
+			
 		} catch(JSONDecodeException $e) {
 			
-		  echo "\n JSONDecodeException - ", $e->getMessage(), (int)$e->getCode();
+		  echo "\n JSONDecodeException - ", htmlspecialchars($e->getMessage(), ENT_QUOTES), (int)$e->getCode();
 		  
 		}
 		
 	}
 	
 	/**
-	 * Returns if the input is a valid JSON string along with content in chosen data type (array or object), when the given input is a valid JSON string.
+	 * Returns if the input is a valid JSON string along with  type, when the given parameter is a valid JSON string.
 	 */
 	public function getJsonDecodedData($value, bool $asArray) {
 		
 		try {
 			
 			$response = [];
-		
-			if (!is_string($value)) {
-				// A non-string value is never a Valid JSON string.
+			
+			$response["is_json"] = false;
+			$response["content"] = "";
 				
-				$response["is_json"] = false;
-				$response["content"] = "";
-				
-				return $response;
-				
-			} else if (is_numeric($value)) { 
-				// A numeric string in double quotes that is encapsulated in single quotes is a valid JSON string. Anything else is never a Valid JSON string.
-				$response["is_json"] = false;
-				$response["content"] = "";
-				
-				return $response;
-				
-			} else if (mb_strlen($value) < 2) { 
-				// Any non-numeric JSON string must be longer than 2 characters, or else, it cannot be considered for next level of JSON string validation checks.
-				$response["is_json"] = false;
-				$response["content"] = "";
+			/**
+			 * Considerations
+			 * A non-string value is never a Valid JSON string.
+			 * A numeric string in double quotes that is encapsulated in single quotes is a valid JSON string. Anything else is never a Valid JSON string.
+			 * Any non-numeric JSON string must be longer than 2 characters, or else, it cannot be considered for next level of JSON string validation checks.
+			 * "null" is never a valid JSON string.
+			 * "true" is never a valid JSON string.
+			 * "false" is never a valid JSON string.
+			 * Every JSON string has to be wrapped in {}, [] or "", if it is not, then that is never a Valid JSON string.
+			 */
+			if ((!is_string($value)) || (is_numeric($value)) || (mb_strlen($value) < 2) || ('null' === $value) || ('true' === $value) || ('false' === $value) || ('{' != $value[0] && '[' != $value[0] && '"' != $value[0])) {
 				
 				return $response;
 				
-			} else if ('null' === $value) { 
-				// "null" is never a valid JSON string.
-				$response["is_json"] = false;
-				$response["content"] = "";
+			}
+			
+			//Actual JSON Decode operation using php's inbuilt json_decode function.				
+			if ($asArray === true) {
 				
-				return $response;
+				$json = json_decode($value, true);
 				
-			} else if ('true' === $value) { 
-				// "true" is never a valid JSON string.
-				$response["is_json"] = false;
-				$response["content"] = "";
+			} else {
 				
-				return $response;
+				$json = json_decode($value);
 				
-			} else if ('false' === $value) { 
-				// "false" is never a valid JSON string.
-				$response["is_json"] = false;
-				$response["content"] = "";
+			}
+			
+			if (($json !== false) && ($json !== null) && ($value != $json)) {
 				
-				return $response;
-				
-			} else if ( '{' != $value[0] && '[' != $value[0] && '"' != $value[0] ) { 
-				// Every JSON string has to be wrapped in {}, [] or "", if it is not, then that is never a Valid JSON string.
-				$response["is_json"] = false;
-				$response["content"] = "";
+				$response["is_json"] = true;
+				$response["content"] = $json;
 				
 				return $response;
 				
 			} else {
-				//Actual JSON Decode operation using php's inbuilt json_decode function.
 				
-				if ($asArray === true) {
-					
-					$json = json_decode($value, true);
-					
-				} else {
-					
-					$json = json_decode($value);
-					
-				}
+				return $response;
 				
-				if (($json !== false) && (!is_null($json)) && ($value != $json)) {
-					
-					$response["is_json"] = true;
-					$response["content"] = $json;
-					
-					return $response;
-					
-				} else {
-					
-					$response["is_json"] = false;
-					$response["content"] = "";
-					
-					return $response;
-					
-				}
 			}
 			
 		} catch(JSONDecodeException $e) {
 			
-		  echo "\n JSONDecodeException - ", $e->getMessage(), (int)$e->getCode();
+		  echo "\n JSONDecodeException - ", htmlspecialchars($e->getMessage(), ENT_QUOTES), (int)$e->getCode();
 		  
 		}
 		
